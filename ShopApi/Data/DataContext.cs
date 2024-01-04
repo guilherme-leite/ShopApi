35db65b3171ba089f32f8a8e91ea5877018ff54b
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 11:24:45 -0300

    Feat: Implementing and passing the options through DataContext.

--- ShopApi/Data/DataContext.cs
+++ ShopApi/Data/DataContext.cs
@@ -4,7 +4,12 @@ using ShopApi.Models;
 namespace ShopApi.Data
 {
     public class DataContext : DbContext
-    {
+    {   
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options) 
+        { 
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
     }
