2d1ad56c210736792c29f21b6c33169544c10f2c
Guilherme Leite <guilhermelax@hotmail.com>
2024-01-04 10:55:39 -0300

    Creating OrderController and implementing the dataContext in the program.cs

---
+++ ShopApi/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApi.Models;
+using ShopApi.Repositories;
+
+namespace ShopApi.Controllers
+{
+    [ApiController]
+    [Route("/orders")]
+    public class OrderController : ControllerBase
+    {
+        public Order Post(
+            [FromServices] ICustomerRepository custumerRepository,
+            [FromServices] IOrderRepository orderRepository)
+        {
+            try {
+            var customer = new Customer { Name = "Guilherme Leite" };
+            var order = new Order { Number = "1", Customer = customer };
+
+            custumerRepository.Save(customer);
+            orderRepository.Save(order);
+
+            return order;
+
+            }catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
--- ShopApi/Program.cs
+++ ShopApi/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ShopApi.Data;
+using ShopApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Database"));
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
